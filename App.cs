c17fc626eb02feccf30febaf40b17316d6e9e0e9
Luu Hong <donkeizluv@gmail.com>
2020-09-12 12:24:59 +0700

    inital commit

---
+++ App.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleTemplate
+{
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+        public App(ILogger<App> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Run()
+        {
+            //await Task.Delay(1000);
+            _logger.LogTrace("Hello!");
+        }
+    }
+}
