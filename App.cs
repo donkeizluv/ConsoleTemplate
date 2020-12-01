4866a864b3e48d7f435645d4e7ca132e33a5a4cd
Luu Hong <donkeizluv@gmail.com>
2020-12-01 10:07:04 +0700

    - update packages - add StyleCop.Analyzers

--- App.cs
+++ App.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-
-namespace ConsoleTemplate
+﻿namespace ConsoleTemplate
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// App DI root.
+    /// </summary>
     public class App
     {
-        private readonly ILogger<App> _logger;
+        private readonly ILogger<App> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        /// <param name="logger">logger implementation.</param>
         public App(ILogger<App> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
+
+        /// <summary>
+        /// Start app main routine.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         public async Task Run()
         {
-            //await Task.Delay(1000);
-            _logger.LogTrace("Hello!");
+            throw new NotImplementedException();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,17 +1,55 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-
-namespace ConsoleTemplate
+﻿namespace ConsoleTemplate
 {
-    static class Program
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using NLog;
+    using NLog.Extensions.Logging;
+
+    /// <summary>
+    /// Static program bootstrap.
+    /// </summary>
+    internal static class Program
     {
-        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
-        private const string APPSETTING_NAME = "appsettings.json";
+        private const string APPSETTINGNAME = "appsettings.json";
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Main app entry.
+        /// </summary>
+        /// <param name="args">start arguments.</param>
+        /// <returns>async task of program entry.</returns>
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("::::::::::::::::::: START :::::::::::::::::::");
+            try
+            {
+                var config = GetConfiguration(APPSETTINGNAME);
+                LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+
+                var servicesProvider = BuildServiceProvider(config);
+                using (servicesProvider as IDisposable)
+                {
+                    var app = servicesProvider.GetRequiredService<App>();
+                    await app.Run();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                LogManager.Shutdown();
+                // Console.ReadLine();
+            }
+
+            Console.WriteLine("::::::::::::::::::: END :::::::::::::::::::");
+        }
+
         private static IServiceProvider BuildServiceProvider(IConfiguration config)
         {
             return new ServiceCollection()
@@ -27,40 +65,13 @@ namespace ConsoleTemplate
                })
                .BuildServiceProvider();
         }
+
         private static IConfiguration GetConfiguration(string configName)
         {
             return new ConfigurationBuilder()
                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
                    .AddJsonFile(configName, optional: true, reloadOnChange: true)
                    .Build();
-        }
-        public static async Task Main(string[] args)
-        {
-            Console.WriteLine("::::::::::::::::::: START :::::::::::::::::::");
-            try
-            {
-                var config = GetConfiguration(APPSETTING_NAME);
-                LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
-
-                var servicesProvider = BuildServiceProvider(config);
-                using (servicesProvider as IDisposable)
-                {
-                    var app = servicesProvider.GetRequiredService<App>();
-                    await app.Run();
-                }
-
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                LogManager.Shutdown();
-                //Console.ReadLine();
-            }
-            Console.WriteLine("::::::::::::::::::: END :::::::::::::::::::");
         }
     }
 }
